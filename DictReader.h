df42a08b56c5e2c1a5448bf626053c2d5be6cb32
Ivan Shapovalov aka intelfx <intelfx100@gmail.com>
2011-12-28 01:42:18 +0400

    New dictionary file format.
    Dictionary file format is changed to that used in Universal Dictionary
    database (http://www.dicts.info).

--- DictReader.h
+++ DictReader.h
@@ -15,91 +15,74 @@
 
 FILE* dictionary;
 
+void helper_tokenize_string (wchar_t* string, std::vector<wchar_t*>& dest, const wchar_t* delim)
+{
+	wchar_t* tmp_ptr;
+	dest.clear();
+
+	while (wchar_t* token = wcstok (string, delim, &tmp_ptr))
+	{
+		dest.push_back (token);
+		string = 0;
+	}
+}
+
 Dictionary read_dictionary() /* LOCALE */
 {
 	__sassert (dictionary, "Error in dictionary file");
 	rewind (dictionary);
 
-	size_t line_num = 0, successful_num = 0;
-	wchar_t input_buf[STATIC_LENGTH], data_buf[2][STATIC_LENGTH];
-	wchar_t designator[STATIC_LENGTH];
+	size_t line_num = 0;
+	wchar_t input_buf[STATIC_LENGTH];
+
 	Dictionary result (&hasher_xroll_plain, 0x100001);
 
-	swprintf (designator, STATIC_LENGTH, L"== %s-%s", langs[cfg.src], langs[cfg.dest]);
+	const wchar_t* delim_stage_1 = L"\t";
+	const wchar_t* delim_stage_2 = L";";
+	wchar_t* tmp_ptr;
 
-	while (!feof (dictionary))
+	std::vector<wchar_t*> tokens_src, tokens_tran;
+
+	while (fgetws (input_buf, STATIC_LENGTH, dictionary))
 	{
 		++line_num;
-
-		fgetws (input_buf, STATIC_LENGTH, dictionary);
 		wstr_tolower (input_buf);
 
-		if (!wcscmp (designator, input_buf)) break; /* advance to section read */
+		if (wchar_t* cmt = wcschr (input_buf, L'#'))
+			*cmt = L'\0';
+
+		wchar_t* sec_src = wcstok (input_buf, delim_stage_1, &tmp_ptr);
+
+		if (!sec_src)
+			continue; /* empty line */
+
+		wchar_t* sec_tran = wcstok (0, delim_stage_1, &tmp_ptr);
+		wchar_t* sec_part = wcstok (0, delim_stage_1, &tmp_ptr);
+
+		bool parse_ok = sec_src && sec_tran && *sec_src && *sec_tran;
+
+		__sverify (!cfg.dictionary_error_fatal || parse_ok, "Malformed dictionary line %zu: \"%ls\" - \"%ls\" - \"%ls\"",
+				   line_num, sec_src, sec_tran, sec_part);
+
+		if (!parse_ok)
+			smsg (E_WARNING, E_VERBOSE, "Malformed dictionary line %zu: \"%ls\" - \"%s\" - \"%s\"",
+				  line_num, sec_src, sec_tran, sec_part);
+
+		helper_tokenize_string (sec_src, tokens_src, delim_stage_2);
+		helper_tokenize_string (sec_tran, tokens_tran, delim_stage_2);
+
+		for (auto srci = tokens_src.begin(); srci != tokens_src.end(); ++srci)
+		{
+// 			if (wcschr (*srci, L' ')) continue; // we do not work with multiword sources
+
+			for (auto trni = tokens_tran.begin(); trni != tokens_tran.end(); ++trni)
+			{
+				smsg (E_INFO, E_DEBUG, "Adding \"%ls\" -> \"%ls\"", *srci, *trni);
+				result.Add (*srci, *trni, !cfg.check_for_multi_insertion);
+			}
+		}
 	}
 
-	__sverify (!feof (dictionary), "Could not find \"%s-%s\" section in dictionary file",
-			   langs[cfg.src], langs[cfg.dest]);
-
-	smsg (E_INFO, E_VERBOSE, "Reading \"%s-%s\" section from line %zu",
-		  langs[cfg.src], langs[cfg.dest], line_num + 1);
-
-	while (!feof (dictionary))
-	{
-		++line_num;
-		++successful_num;
-
-		wchar_t* gets_r = fgetws (input_buf, STATIC_LENGTH, dictionary);
-		__sverify (gets_r, "fgetws() failed, invalid characters detected in dictionary file [line %zu]", line_num);
-		wstr_tolower (input_buf);
-
-		if (!wcsncmp (input_buf, designator, 2)) break; /* stop section read */
-
-		if (! (line_num % 100))
-			smsg (E_INFO, E_DEBUG, "Adding %zu-th line", line_num);
-
-		data_buf[1][0] = L'\0';
-		data_buf[0][0] = L'\0'; /* guard */
-
-		int scanf_result = swscanf (input_buf, L"%l[^=]=%l[^=]", data_buf[0], data_buf[1]);
-
-		if (scanf_result == EOF)
-		{
-			__sverify (!ferror (dictionary), "I/O error on line %zu: %s", line_num, strerror (errno));
-			/* empty line, skip it */
-		}
-
-		else
-		{
-			bool parse_ok = (scanf_result == 2) && data_buf[0][0] && data_buf[1][0];
-
-			__sverify (!cfg.dictionary_error_fatal || parse_ok,
-					   "Malformed dictionary line %zu: [sscanf returned %d] \"%ls\"",
-					   line_num, scanf_result, input_buf);
-
-			if (!parse_ok)
-				smsg (E_WARNING, E_VERBOSE, "Malformed dictionary line %zu: [sscanf returned %d] \"%ls\"",
-					  line_num, scanf_result, input_buf);
-
-			else
-			{
-				{
-					wchar_t* ptr = data_buf[1];
-					while (*ptr++)
-						if (*ptr == L'\t')
-							*ptr = L'|';
-				}
-
-				smsg (E_INFO, E_DEBUG, "Adding pair: \"%ls\" -> \"%ls\"",
-					  data_buf[0],
-					  data_buf[1]);
-
-				result.Add (data_buf[0], data_buf[1], !cfg.check_for_multi_insertion);
-			}
-		} /* scanf_result != EOF */
-	} /* while (!feof) */
-
-	smsg (E_INFO, E_DEBUG, "Read %zu lines of section \"%s-%s\"",
-		  successful_num, langs[cfg.src], langs[cfg.dest]);
 	return std::move (result);
 }
 
