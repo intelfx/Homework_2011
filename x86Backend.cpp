1791b7164eb8078ccd9850f18eb6851b0140e7ea
Ivan Shapovalov <intelfx100@gmail.com>
2012-10-12 10:03:02 +0400

    Add the common JIT compiler parts.
    JIT is not ready; compilation will break.
    Though, these are all architecture-agnostic parts of JIT which are OK to
    commit separately in order to do further modularization.

---
+++ x86Backend.cpp
@@ -0,0 +1,123 @@
+#include "stdafx.h"
+#include "x86Backend.h"
+
+// -------------------------------------------------------------------------------------
+// Library:		Homework
+// File:		x86Backend.h
+// Author:		Ivan Shapovalov <intelfx100@gmail.com>
+// Description:	x86 JIT compiler backend.
+// -------------------------------------------------------------------------------------
+
+namespace ProcessorImplementation
+{
+using namespace Processor;
+
+bool x86Backend::ImageIsOK( size_t chk )
+{
+	msg( E_INFO, E_DEBUG, "Verifying image for checksum %zx", chk );
+
+	auto it = images_.find( chk );
+	if( it == images_.end() ) {
+		msg( E_WARNING, E_DEBUG, "Image for checksum %zx does not exist", chk );
+		return false;
+	}
+	if( !it->second.mm.length ) {
+		msg( E_WARNING, E_DEBUG, "Image for checksum %zx is empty", chk );
+		return false;
+	}
+	if( !it->second.mm.image ) {
+		msg( E_WARNING, E_DEBUG, "Image for checksum %zx is not finalized", chk );
+		return false;
+	}
+	if( it->second.data.size() != it->second.mm.length ) {
+		msg( E_WARNING, E_DEBUG, "Image for checksum %zx is out of sync", chk );
+		return false;
+	}
+	return true;
+}
+
+bool x86Backend::_Verify() const
+{
+	verify_statement( current_image_, "No image is selected" );
+	auto it = images_.find( current_chk_ );
+	verify_statement( it != images_.end(), "Image not found for the current checksum %zx", current_chk_ );
+	verify_statement( &it->second == current_image_, "Looked up image does not match selected (chk %zx)", current_chk_ );
+	return true;
+}
+
+void* x86Backend::GetImage( size_t chk )
+{
+	cassert( ImageIsOK( chk ), "Image for checksum %zx is not good/ready", chk );
+	return images_.find( chk )->second.mm.image;
+}
+
+void x86Backend::Select( size_t chk, bool create )
+{
+	auto it = images_.find( chk );
+	if( it == images_.end() ) {
+		cassert( create, "Selecting inexistent image chk %zx when create == false", chk );
+		it = images_.insert( std::make_pair( chk, NativeImage() ) ).first;
+	}
+	current_chk_ = chk;
+	current_image_ = &it->second;
+
+	verify_method;
+}
+
+void x86Backend::Deallocate()
+{
+	verify_method;
+
+	if( current_image_->mm.image ) {
+		proc_->ExecutionManager().DeallocateMemory( current_image_->mm.image, current_image_->mm.length );
+		current_image_->mm.image = 0;
+	}
+}
+
+void x86Backend::Finalize()
+{
+	verify_method;
+
+	Deallocate();
+
+	void* img = proc_->ExecutionManager().AllocateMemory( current_image_->data.size(), true, true, true );
+	cassert( img, "Failed to allocate memory through native execution manager (size: %zu, readable, writable, executable)",
+			 current_image_->data.size() );
+	memcpy( img, current_image_->data, current_image_->data.size() );
+	current_image_->mm.image = img;
+
+	cassert( ImageIsOK( current_chk_ ), "Newly finalized image is not OK - inconsistency" );
+}
+
+void x86Backend::Clear()
+{
+	verify_method;
+
+	Deallocate();
+	current_image_->data.clear();
+}
+
+void x86Backend::CompileBuffer( size_t chk, abi_callback_fn_t callback )
+{
+	Select( chk, true );
+	Clear();
+	current_image_->callback = callback;
+
+	IMMU* mmu = proc_->MMU();
+	ILogic* logic = proc_->LogicProvider();
+
+	size_t section_limits[SEC_MAX];
+	mmu->QueryLimits( section_limits );
+
+	for( size_t pc = 0; pc < section_limits[SEC_CODE_IMAGE]; ++pc ) {
+		Command& cmd = mmu->ACommand( pc );
+		msg( E_INFO, E_DEBUG, "Emitting native code for command [PC=%zu] \"%s\"",
+			 pc, logic->DumpCommand( cmd ) );
+		CompileCommand( cmd );
+	}
+
+	msg( E_INFO, E_VERBOSE, "Native code emission completed" );
+	Finalize();
+}
+
+} // namespace ProcessorImplementation
---
+++ x86Backend.h
@@ -0,0 +1,54 @@
+#ifndef INTERPRETER_X86BACKEND_H
+#define INTERPRETER_X86BACKEND_H
+
+#include "build.h"
+
+#include "Interfaces.h"
+#include <uXray/fxcontainers.h>
+
+// -------------------------------------------------------------------------------------
+// Library:		Homework
+// File:		x86Backend.h
+// Author:		Ivan Shapovalov <intelfx100@gmail.com>
+// Description:	x86 JIT compiler backend.
+// -------------------------------------------------------------------------------------
+
+namespace ProcessorImplementation
+{
+using namespace Processor;
+
+class x86Backend : public IBackend
+{
+	struct NativeImage
+	{
+		llarray data; // primary working buffer
+		struct // mapped executable region
+		{
+			void* image; 
+			size_t length;
+		} mm;
+		abi_callback_fn_t callback; // processor API callback function
+	};
+	std::map<size_t, NativeImage> images_;
+	size_t current_chk_;
+	NativeImage* current_image_;
+
+	void Select( size_t chk, bool create = false );
+	void Finalize();
+	void Deallocate();
+	void Clear();
+
+	void CompileCommand( Command& cmd );
+
+protected:
+	virtual bool _Verify() const;
+	
+public:
+	virtual void CompileBuffer( size_t chk, abi_callback_fn_t callback );
+	virtual void* GetImage( size_t chk );
+	virtual bool ImageIsOK( size_t chk );
+};
+
+} // namespace ProcessorImplementation
+
+#endif // INTERPRETER_X86BACKEND_H
